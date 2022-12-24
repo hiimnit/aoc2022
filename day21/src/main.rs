da2b5e48162505bbcda45924b521040ce85c42bf
Filip Soukup <fsoukup@artex-is.cz>
2022-12-24 23:54:07 +0100

    day 21 - part 2

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -27,7 +27,7 @@ impl Operation {
 enum Job<'a> {
     Yell(i64),
     Calculate(&'a str, Operation, &'a str),
-    // TODO HumanDependant
+    Human,
 }
 
 fn main() {
@@ -59,15 +59,8 @@ fn main() {
         monkeys.insert(monkey, job);
     }
 
-    // println!("digraph G {{");
-    // println!("\thumn [shape=Mdiamond];");
-    // for (monkey, job) in &monkeys {
-    //     if let Job::Calculate(m1, _, m2) = job {
-    //         println!("\t{m1} -> {monkey};");
-    //         println!("\t{m2} -> {monkey};");
-    //     }
-    // }
-    // println!("}}");
+    // comment this line to get the part 1 solution
+    *monkeys.get_mut("humn").unwrap() = Job::Human;
 
     let depths = calc_depths(&monkeys);
 
@@ -75,33 +68,55 @@ fn main() {
     sorted_depths.sort();
     for depth in sorted_depths {
         for monkey in &depths[depth] {
-            let result: i64 = match &monkeys[monkey] {
-                Job::Yell(n) => n.to_owned(),
+            let result: Option<i64> = match &monkeys[monkey] {
+                Job::Yell(n) => Some(n.to_owned()),
+                Job::Human => None,
                 Job::Calculate(m1, operation, m2) => {
                     let operand1 = match &monkeys[m1] {
-                        Job::Yell(n) => n,
-                        _ => unimplemented!("Monkey {m1} has not yelled its number yet!"),
+                        Job::Yell(n) => Some(n),
+                        _ => None,
                     };
                     let operand2 = match &monkeys[m2] {
-                        Job::Yell(n) => n,
-                        _ => unimplemented!("Monkey {m2} has not yelled its number yet!"),
+                        Job::Yell(n) => Some(n),
+                        _ => None,
                     };
 
-                    match operation {
-                        Operation::Add => operand1 + operand2,
-                        Operation::Subtract => operand1 - operand2,
-                        Operation::Multiply => operand1 * operand2,
-                        Operation::Divide => operand1 / operand2,
+                    match (operand1, operand2) {
+                        (Some(operand1), Some(operand2)) => Some(match operation {
+                            Operation::Add => operand1 + operand2,
+                            Operation::Subtract => operand1 - operand2,
+                            Operation::Multiply => operand1 * operand2,
+                            Operation::Divide => operand1 / operand2,
+                        }),
+                        _ => None,
                     }
                 }
             };
 
-            *monkeys.get_mut(monkey).unwrap() = Job::Yell(result);
+            if let Some(number) = result {
+                *monkeys.get_mut(monkey).unwrap() = Job::Yell(number);
+            }
         }
     }
 
-    println!("root yells {:?}", monkeys.get("root").unwrap());
-    println!("rmtt yells {:?}", monkeys.get("rmtt").unwrap());
+    let root = monkeys.get("root").unwrap();
+
+    match root {
+        Job::Yell(number) => println!("root yells {number}"),
+        Job::Calculate(m1, _, m2) => {
+            let result = match (&monkeys[m1], &monkeys[m2]) {
+                (Job::Yell(expected_number), Job::Calculate(_, _, _)) => {
+                    find_human(&monkeys, m2, *expected_number)
+                }
+                (Job::Calculate(_, _, _), Job::Yell(expected_number)) => {
+                    find_human(&monkeys, m1, *expected_number)
+                }
+                _ => unimplemented!("Unexpected state!"),
+            };
+            println!("human should yell {result}");
+        }
+        Job::Human => unimplemented!("Root should not be the human!"),
+    };
 }
 
 fn calc_depths<'a>(monkeys: &HashMap<&'a str, Job>) -> HashMap<i64, Vec<&'a str>> {
@@ -120,7 +135,36 @@ fn calc_depths<'a>(monkeys: &HashMap<&'a str, Job>) -> HashMap<i64, Vec<&'a str>
 
 fn calc_depth(monkeys: &HashMap<&str, Job>, monkey: &str) -> i64 {
     match monkeys[monkey] {
-        Job::Yell(_) => 0,
+        Job::Yell(_) | Job::Human => 0,
         Job::Calculate(m1, _, m2) => max(calc_depth(monkeys, m1), calc_depth(monkeys, m2)) + 1,
+    }
+}
+
+fn find_human<'a>(
+    monkeys: &'a HashMap<&'a str, Job>,
+    monkey: &'a str,
+    expected_number: i64,
+) -> i64 {
+    let (first_monkey, operation, second_monkey) = match &monkeys[monkey] {
+        Job::Yell(_) => unimplemented!("Unexpected state!"),
+        Job::Calculate(m1, operation, m2) => (m1, operation, m2),
+        Job::Human => return expected_number,
+    };
+
+    match (&monkeys[first_monkey], &monkeys[second_monkey]) {
+        (Job::Yell(n), Job::Calculate(_, _, _)) | (Job::Yell(n), Job::Human) => match operation {
+            Operation::Add => return find_human(monkeys, second_monkey, expected_number - n),
+            Operation::Subtract => return find_human(monkeys, second_monkey, n - expected_number),
+            Operation::Multiply => return find_human(monkeys, second_monkey, expected_number / n),
+            Operation::Divide => return find_human(monkeys, second_monkey, n / expected_number),
+        },
+        (Job::Calculate(_, _, _), Job::Yell(n)) | (Job::Human, Job::Yell(n)) => match operation {
+            Operation::Add => return find_human(monkeys, first_monkey, expected_number - n),
+            Operation::Subtract => return find_human(monkeys, first_monkey, n + expected_number),
+            Operation::Multiply => return find_human(monkeys, first_monkey, expected_number / n),
+            Operation::Divide => return find_human(monkeys, first_monkey, n * expected_number),
+        },
+
+        _ => unimplemented!("Unexpected state!"),
     }
 }
